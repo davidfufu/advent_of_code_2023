ae7e868d9015d86fb747f10211130450be4652ea
David Fu <david.fu.df@gmail.com>
2024-03-15 18:24:16 -0400

    update

---
+++ python/24/24.py
@@ -0,0 +1,65 @@
+import re
+import os
+import time
+
+INPUT_TEXT_PATH = "./24_input.txt"
+TEST_INPUT_TEXT_PATH = "./24_test_input.txt"
+
+
+class ScratchCards:
+    def __init__(self, filePath):
+        # self.partOne()
+        self.partTwo()
+
+    def partOne(self):
+        puzzleInput = open(INPUT_TEXT_PATH).read()
+        total = 0
+        numbersRegex = r":(.*)\|(.*)"
+
+        # for card
+        # print(re.findall(numbersRegex, puzzleInput))
+        for winNums, myNums in re.findall(numbersRegex, puzzleInput):
+            wins = set(myNums.strip().split()) & set(winNums.strip().split())
+            if wins:
+                points = 2 ** (len(wins) - 1)
+                total += points
+
+        print(total)
+
+    def partTwo(self):
+        puzzleInput = open(TEST_INPUT_TEXT_PATH).read()
+        numbersRegex = r"Card\s(\d*):(.*)\|(.*)"
+        cards = re.findall(numbersRegex, puzzleInput)
+        print(type(cards), len(cards))
+        print(cards)
+        totalCards = {}
+
+        def cardCounter(card: tuple, totalCards):
+            print("Evaluating: ", card)
+
+            cardNumber, winNums, myNums = card
+
+            if cardNumber not in totalCards:
+                totalCards[cardNumber] = 1
+            else:
+                totalCards[cardNumber] += 1
+
+            overlaps = self.findOverlaps(winNums, myNums)
+
+            for i in range(int(cardNumber) + 1, int(cardNumber) + len(overlaps) + 1):
+                cardCounter(cards[i - 1], totalCards)
+
+        for card in cards:
+            cardCounter(card, totalCards)
+
+        print(totalCards)
+
+    def findOverlaps(self, winNumStr, myNumStr) -> set:
+        return set(myNumStr.strip().split()) & set(winNumStr.strip().split())
+
+
+if __name__ == "__main__":
+    t1 = time.time()
+    scratchCard = ScratchCards(TEST_INPUT_TEXT_PATH)
+    elapsed = time.time() - t1
+    print("Took: ", elapsed)
