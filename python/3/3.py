4843e0108fe81cbe0e22820cd5076eb79ae44cf6
David Fu <david.fu.df@gmail.com>
2024-03-15 02:22:16 -0400

    update

--- python/3/3.py
+++ python/3/3.py
@@ -1,5 +1,6 @@
 import os
 import pprint
+import re
 
 INPUT_TEXT_PATH = "./3_input.txt"
 INPUT_TEXT_TEST_PATH = "./3_input_test.txt"
@@ -7,32 +8,103 @@ INPUT_TEXT_TEST_PATH = "./3_input_test.txt"
 
 class Machine:
     def __init__(self, input_path):
-        with open(INPUT_TEXT_TEST_PATH, "r") as f:
-            lines = f.readlines()
-            self.schematic2D = [[*line.strip()] for line in lines]
+        with open(INPUT_TEXT_PATH, "r") as f:
+            self.lines = [line.strip() for line in f.readlines()]
+            self.schematic2D = [[*line] for line in self.lines]
 
             self.rowMaxIdx = len(self.schematic2D) - 1
             self.columnMaxIdx = len(self.schematic2D[0]) - 1
 
-            # a set of all the coords adjacent to gears,
-            # if a num coord is in here, it's a gear part
-            self.allGearAdjacentCoords = set()
+            # a set of all the coords adjacent to parts
+            # if a num coord is in here, it's a part
+            # self.allPartAdjacentCoords = set()
 
-            # a list of lists [partNumber, (coord tuple)]
+            # all coords adjacent to gears
+            self.allPartAdjacentCoords = set()
+
+            # # a list of lists [partNumber, (coord tuple)]
             self.numbersAndTheirCoords = []
-
-            self.getAllAdjacentGearCoords()
+            self.allGearAdjacentCoords = set()
+            self.adjacentCoordToGearMapping = {}
+            # self.getAllAdjacentGearCoords()
 
             self.getAllCoordsForEachNumber()
 
-            self.checkWhichNumbersAreAdjacentToGears()
+            # self.checkWhichNumbersAreAdjacentToParts()
 
-    def checkWhichNumbersAreAdjacentToGears(self):
+            self.findGearAdjacentCoords()
+
+            self.findNumbersAdjacentToEachGear()
+
+    def findGearAdjacentCoords(self):
+        gearRegex = r"[\*]"
+        gearAdjacent = set()
+        adjacentCoordToGearMapping = {}
+
+        for i, line in enumerate(self.lines):
+            for m in re.finditer(gearRegex, line):
+                j = m.start()
+
+                for r in range(i - 1, i + 2):
+                    for c in range(j - 1, j + 2):
+                        gearAdjacent.add((r, c))
+                        adjacentCoordToGearMapping[(r, c)] = (i, j)
+
+        self.allGearAdjacentCoords = gearAdjacent
+        self.adjacentCoordToGearMapping = adjacentCoordToGearMapping
+        # print(symbol_adjacent)
+
+    def findNumbersAdjacentToEachGear(self):
+        adjacentNumbersPerGear = {}
+
+        numberRegex = r"\d+"
+        for i, line in enumerate(self.lines):
+            for m in re.finditer(numberRegex, line):
+                print(m.span())
+                allIndexesOfNumber = [(i, j) for j in range(m.span()[0], m.span()[1])]
+
+                # This is a subset of the all indexes of the number that are adjacent to a gear
+                adjacentCoords = [
+                    coord
+                    for coord in allIndexesOfNumber
+                    if coord in self.allGearAdjacentCoords
+                ]
+
+                numberIsBesideAGear = len(adjacentCoords) > 0
+
+                if numberIsBesideAGear:
+                    number = m.group()
+                    coordOfGearItsAdjacentTo = self.adjacentCoordToGearMapping[
+                        adjacentCoords[-1]
+                    ]
+
+                    if coordOfGearItsAdjacentTo not in adjacentNumbersPerGear:
+                        adjacentNumbersPerGear[coordOfGearItsAdjacentTo] = [number]
+                    else:
+                        adjacentNumbersPerGear[coordOfGearItsAdjacentTo].append(number)
+
+                    print(
+                        f"The number {number} is beside the gear at the coord {coordOfGearItsAdjacentTo}"
+                    )
+
+        print(adjacentNumbersPerGear)
+
+        validGears = sum(
+            [
+                int(pair[1][0]) * int(pair[1][1])
+                for pair in adjacentNumbersPerGear.items()
+                if len(pair[1]) == 2
+            ]
+        )
+
+        print(validGears)
+
+    def checkWhichNumbersAreAdjacentToParts(self):
         sum = 0
         for numberData in self.numbersAndTheirCoords:
             number, coords = numberData
             for coord in coords:
-                if coord in self.allGearAdjacentCoords:
+                if coord in self.allPartAdjacentCoords:
                     print("ADDING: ", number)
                     sum += int(number)
                     break
@@ -97,7 +169,7 @@ class Machine:
                         self.symbols.append(char)
 
                     adjacentCoords = self.finalAllAdjacentCoords(row_idx, column_idx)
-                    self.allGearAdjacentCoords.update(adjacentCoords)
+                    self.allPartAdjacentCoords.update(adjacentCoords)
 
                     # print(f"for {char} at {row_idx},{column_idx} there are {len(adjacentCoords)} adjacent coords")
                     # print(adjacentCoords)
@@ -105,4 +177,4 @@ class Machine:
 
 if __name__ == "__main__":
     machine = Machine(INPUT_TEXT_PATH)
-    # https://aoc-puzzle-solver.streamlit.app/
+# https://aoc-puzzle-solver.streamlit.app/
